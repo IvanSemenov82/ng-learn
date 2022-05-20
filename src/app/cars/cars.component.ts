47d983acb5c9dd3afe77072b4009de28d49039bf
IvanSemenov82 <ivsemenov82@gmail.com>
2022-05-20 16:03:56 +0300

    *ngIf

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -7,39 +7,11 @@ import { Component } from '@angular/core';
 })
 export class CarsComponent {
 
-  canAddCar = false;
-
-  constructor() {
-
-    // 1.
-    // setTimeout( () => {
-    //   this.canAddCar = true;
-    // }, 3000)
-
-  }
-
-  // 2.
-  addCarStatus = '';
-  inputText = 'Default text'
+  carName = '';
+  addCarStatus = false;
 
   addCar() {
-    this.addCarStatus = "Машина добавлена";
-  }
-
-  // 1.
-  // onKeyUp(event : Event) {
-  //   this.inputText = (<HTMLInputElement>event.target).value;
-  // }
-  // 2.
-  // onKeyUp(value : any) {
-  //   this.inputText = value;
-  // }
-  // onKeyUp(event : KeyboardEvent) {
-  //   if (event.code === "Enter")
-  //     this.inputText = (<HTMLInputElement>event.target).value;
-  // }
-  onKeyUp(event : Event) {
-      this.inputText = (<HTMLInputElement>event.target).value;
+    this.addCarStatus = true;
   }
 
 }
