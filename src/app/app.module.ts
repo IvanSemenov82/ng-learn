6d7d0705aa9622d011454de8dee541bd172453d3
IvanSemenov82 <ivsemenov82@gmail.com>
2022-05-23 22:13:16 +0300

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CarComponent } from './car/car.component';
 import { CarsComponent } from './cars/cars.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     CarComponent,
-    CarsComponent
+    CarsComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/cars/cars.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent {
+
+  carName = '';
+  carYear = 2017;
+
+  cars: {name: string, year: number} [] = [
+    {
+      name : 'Ford',
+      year : 2015
+    },
+    {
+      name : 'Mazda',
+      year : 2010
+    },
+    {
+      name : 'Audi',
+      year : 2005
+    }
+  ]
+
+  addCar() {
+    this.cars.push({
+      name: this.carName,
+      year: this.carYear
+    });
+    this.carName = '';
+    this.carYear = 2017;
+  }
+
+}
