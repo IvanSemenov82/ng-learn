8e1800ca3038d44a5f7f4dd6f83a7d26f8586f39
IvanSemenov82 <ivsemenov82@gmail.com>
2022-07-10 15:05:32 +0300

    Passing parameters to the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
+export interface Skill {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
 
+  posts: Post[] = [
+    {
+      title: 'Первый пост',
+      text: 'Какойто тeкст',
+      id: 1,
+    },
+    {
+      title: 'Второй пост',
+      text: 'Ещё какойто текст',
+      id: 2,
+    }
+  ];
+
+  skills: Skill[] = [
+    {
+      title: 'Первый навык',
+      text: 'HTML',
+      id: 1,
+    },
+    {
+      title: 'Второй навык',
+      text: 'CSS',
+      id: 1,
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {PostFormComponent} from './post-form/post-form.component';
 import {PostComponent} from './post/post.component';
+import { MyComponent } from './my/my.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostFormComponent,
     PostComponent,
+    MyComponent,
   ],
   imports: [
     BrowserModule,
